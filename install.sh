82145f1794afbd3b3a21a2eba16a45eb48e66211
Pedro Mangabeira <MaethorNaur@users.noreply.github.com>
2020-10-10 17:55:12 +0100

    update

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
 go get -u github.com/arl/gitmux
+sudo /usr/bin/tic -xe alacritty-direct,tmux-256color terminfo.src
 sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
+infocmp -x alacritty-direct
